f9fa8fe1b2ec3fce5aaf92f07970581c503059a5
Maksim Gorshkov <m.gorshkov96@gmail.com>
2023-05-20 17:26:51 +0300

    Completed right lesson02.exercise01

--- ITMO.124-28.Cpp.Lesson02/ITMO.124-28.Cpp.Lesson02.Exercise01/ITMO.124-28.Cpp.Lesson02.Exercise01.cpp
+++ ITMO.124-28.Cpp.Lesson02/ITMO.124-28.Cpp.Lesson02.Exercise01/ITMO.124-28.Cpp.Lesson02.Exercise01.cpp
@@ -1,27 +1,120 @@
 #include <iostream>
 
+bool checkPrimeNumber(int number);
+
 int main()
 {   
-    int year;
-    bool answer;
-    
-    std::cout << "Enter year: ";
-    std::cin >> year;
+    int checkingNumber;       
+    /*int counter = 0;
+    int counter2 = 0;
+    int counter3 = 0;*/
+    int primeNumber = 0;
 
-    // First variant
-    std::cout << "Answer by first variant: ";
-    if (year % 4 == 0 && year % 100 != 0 || year % 400 == 0)
-    {               
-        std::cout << "Yes\n";            
+    bool isPrime;
+    
+
+    std::cout << "Enter number: ";
+    std::cin >> checkingNumber;
+
+    isPrime = checkPrimeNumber(checkingNumber);
+    
+    if (isPrime)
+    {
+        for (int i = 2; i <= checkingNumber; i++)
+        {
+            isPrime = checkPrimeNumber(i);
+
+            if (isPrime)
+            {
+                primeNumber++;
+            }
+        }
+
+        isPrime = checkPrimeNumber(primeNumber);
+
+        if (isPrime)
+        {
+            std::cout << "Yes" << std::endl;
+        }
+        else
+        {
+            std::cout << "No" << std::endl;
+        }
+    }
+    else
+    {
+        std::cout << "No" << std::endl;
+    }
+
+    /*for (int i = 1; i <= checkingNumber; i++) 
+    {        
+        if (checkingNumber % i == 0) 
+        {
+            counter++;
+        }        
+    }
+    
+    if (counter == 2) 
+    {
+        for (int i = 2; i <= checkingNumber; i++) 
+        {
+            counter2 = 0;
+            for (int j = 1; j <= i; j++) 
+            {               
+                if (i % j == 0)
+                {
+                    counter2++;
+                }               
+            }
+            if (counter2 == 2)
+            {
+                primeNumber++;
+            }
+        }
+
+        for (int i = 1; i <= primeNumber; i++)
+        {
+            if (primeNumber % i == 0)
+            {
+                counter3++;
+            }
+        }
+
+        if (counter3 == 2) 
+        {
+            std::cout << "Yes" << std::endl;
+        }
+        else 
+        {
+            std::cout << "No" << std::endl;
+        }
     }
     else 
     {
-        std::cout << "No\n";
+        std::cout << "No" << std::endl;
+    }*/
+
+    
+}
+
+bool checkPrimeNumber(int number)
+{
+    int counter = 0;
+
+    for (int i = 1; i <= number; i++)
+    {
+        if (number % i == 0)
+        {
+            counter++;
+        }
     }
 
-    //Second variant
-    std::cout << "Answer by second variant: ";
-    answer = 1 * (year % 4 == 0 && year % 100 != 0 || year % 400 == 0);
-    std::cout << answer << std::endl;
-    return 0;
+    if (counter == 2)
+    {
+        return true;
+    }
+    else
+    {
+        return false;
+    }   
 }
