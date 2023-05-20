436798d17fdccafa6a7082766f27961720bc34e7
Maksim Gorshkov <m.gorshkov96@gmail.com>
2023-05-20 17:27:56 +0300

    Deleted comments in lesson02.exercise01

--- ITMO.124-28.Cpp.Lesson02/ITMO.124-28.Cpp.Lesson02.Exercise01/ITMO.124-28.Cpp.Lesson02.Exercise01.cpp
+++ ITMO.124-28.Cpp.Lesson02/ITMO.124-28.Cpp.Lesson02.Exercise01/ITMO.124-28.Cpp.Lesson02.Exercise01.cpp
@@ -5,13 +5,8 @@ bool checkPrimeNumber(int number);
 int main()
 {   
     int checkingNumber;       
-    /*int counter = 0;
-    int counter2 = 0;
-    int counter3 = 0;*/
-    int primeNumber = 0;
-
     bool isPrime;
-    
+    int primeNumber = 0;      
 
     std::cout << "Enter number: ";
     std::cin >> checkingNumber;
@@ -44,57 +39,7 @@ int main()
     else
     {
         std::cout << "No" << std::endl;
-    }
-
-    /*for (int i = 1; i <= checkingNumber; i++) 
-    {        
-        if (checkingNumber % i == 0) 
-        {
-            counter++;
-        }        
-    }
-    
-    if (counter == 2) 
-    {
-        for (int i = 2; i <= checkingNumber; i++) 
-        {
-            counter2 = 0;
-            for (int j = 1; j <= i; j++) 
-            {               
-                if (i % j == 0)
-                {
-                    counter2++;
-                }               
-            }
-            if (counter2 == 2)
-            {
-                primeNumber++;
-            }
-        }
-
-        for (int i = 1; i <= primeNumber; i++)
-        {
-            if (primeNumber % i == 0)
-            {
-                counter3++;
-            }
-        }
-
-        if (counter3 == 2) 
-        {
-            std::cout << "Yes" << std::endl;
-        }
-        else 
-        {
-            std::cout << "No" << std::endl;
-        }
-    }
-    else 
-    {
-        std::cout << "No" << std::endl;
-    }*/
-
-    
+    }     
 }
 
 bool checkPrimeNumber(int number)
