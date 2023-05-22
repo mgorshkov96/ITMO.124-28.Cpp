5a851abf5358b6f9e7add9180de2dee739f421f1
Maksim Gorshkov <m.gorshkov96@gmail.com>
2023-05-22 16:35:33 +0300

    Completed lesson02.exercise02

--- ITMO.124-28.Cpp.Lesson02/ITMO.124-28.Cpp.Lesson02.Exercise02/ITMO.124-28.Cpp.Lesson02.Exercise02.cpp
+++ ITMO.124-28.Cpp.Lesson02/ITMO.124-28.Cpp.Lesson02.Exercise02/ITMO.124-28.Cpp.Lesson02.Exercise02.cpp
@@ -1,20 +1,63 @@
-// ITMO.124-28.Cpp.Lesson02.Exercise02.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <iostream>
+#include <array>
+#include <string>
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    long long number;
+    int numbers[11];
+    int mainNumbers[9];
+    int checkNumber;
+    int multMainNumbers = 0;
+    
+    std::cout << "Enter number\n";
+    std::cin >> number;
+
+    numbers[0] = number / 10000000000;
+    numbers[1] = (number % 10000000000) / 1000000000;
+    numbers[2] = (number % 1000000000) / 100000000;
+    numbers[3] = (number % 100000000) / 10000000;
+    numbers[4] = (number % 10000000) / 1000000;
+    numbers[5] = (number % 1000000) / 100000;
+    numbers[6] = (number % 100000) / 10000;
+    numbers[7] = (number % 10000) / 1000;
+    numbers[8] = (number % 1000) / 100;
+    numbers[9] = (number % 100) / 10;
+    numbers[10] = number % 10;
+    
+    for (int i = 0; i < 9; i++)
+    {
+        mainNumbers[i] = numbers[i];
+    }
+
+    std::string strCheckNumber = std::to_string(numbers[9]) + std::to_string(numbers[10]);
+    checkNumber = std::stoi(strCheckNumber);
+
+    for (int i = 0, j = std::size(mainNumbers); i < std::size(mainNumbers); i++, j--)
+    {
+        multMainNumbers = multMainNumbers + mainNumbers[i] * j;
+    }
+
+    if (multMainNumbers == 100 || multMainNumbers == 101)
+    {
+        multMainNumbers = 00;
+    }
+    else if (multMainNumbers > 101)
+    {
+        multMainNumbers = multMainNumbers % 101;
+
+        if (multMainNumbers == 100)
+        {
+            multMainNumbers = 00;
+        }
+    }
+
+    if (multMainNumbers == checkNumber)
+    {
+        std::cout << "True";
+    }
+    else 
+    {
+        std::cout << "False";
+    }
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
