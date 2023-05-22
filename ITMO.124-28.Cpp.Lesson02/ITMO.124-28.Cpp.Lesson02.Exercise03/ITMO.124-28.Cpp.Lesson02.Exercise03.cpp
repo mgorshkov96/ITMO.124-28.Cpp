f10c1e7b1a6a689c6d94179ad3313e9237dde158
Maksim Gorshkov <m.gorshkov96@gmail.com>
2023-05-22 17:45:09 +0300

    Completed lesson02.exercise03

--- ITMO.124-28.Cpp.Lesson02/ITMO.124-28.Cpp.Lesson02.Exercise03/ITMO.124-28.Cpp.Lesson02.Exercise03.cpp
+++ ITMO.124-28.Cpp.Lesson02/ITMO.124-28.Cpp.Lesson02.Exercise03/ITMO.124-28.Cpp.Lesson02.Exercise03.cpp
@@ -2,5 +2,24 @@
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    constexpr int tenCoin = 10;
+    constexpr int fiveCoin = 5;
+    constexpr int twoCoin = 2;
+    constexpr int oneCoin = 1;
+    int numberOfTenCoins;
+    int numberOfFiveCoins;
+    int numberOfTwoCoins;
+    int numberOfOneCoins;
+    int sum;
+
+    std::cout << "Enter sum: ";
+    std::cin >> sum;
+
+    numberOfTenCoins = sum / tenCoin;
+    numberOfFiveCoins = sum % tenCoin / fiveCoin;
+    numberOfTwoCoins = (sum % tenCoin) % fiveCoin / twoCoin;
+    numberOfOneCoins = ((sum % tenCoin) % fiveCoin) % twoCoin / oneCoin;
+
+    std::cout << "Ten Coins\t" << "Five Coins\t" << "Two Coins\t" << "One Coins\n";
+    std::cout << numberOfTenCoins << "\t\t" << numberOfFiveCoins << "\t\t" << numberOfTwoCoins << "\t\t" << numberOfOneCoins;
 }
