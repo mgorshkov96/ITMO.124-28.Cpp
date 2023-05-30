bdf57f5e53887c0b3b7392f2868c4c9b5b66776e
Maksim Gorshkov <m.gorshkov96@gmail.com>
2023-05-30 16:27:24 +0300

    Completed lesson06.exercise02

--- ITMO.124-28.Cpp.Lesson06/ITMO.124-28.Cpp.Lesson06.Exercise02/ITMO.124-28.Cpp.Lesson06.Exercise02.cpp
+++ ITMO.124-28.Cpp.Lesson06/ITMO.124-28.Cpp.Lesson06.Exercise02/ITMO.124-28.Cpp.Lesson06.Exercise02.cpp
@@ -3,5 +3,48 @@
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    const int n = 10;
+    int arr[n] = { 1, 25, 6, 32, 43, 5, 96, 23, 4, 55 };
+    int min = 0;
+    int buf = 0;
+
+    std::ofstream fout("arrays.txt", std::ios::app);
+
+    if (!fout)
+    {
+        std::cerr << "Recording failed" << std::endl;
+    }
+
+    for (int i : arr)
+    {
+        fout << i << " ";
+    }
+
+    fout << "\n";
+
+    for (int i = 0; i < n; i++)
+    {
+        min = i; 
+
+        for (int j = i + 1; j < n; j++)
+        {
+            min = (arr[j] < arr[min]) ? j : min;
+        }            
+        
+        if (i != min)
+        {
+            buf = arr[i];
+            arr[i] = arr[min];
+            arr[min] = buf;
+        }
+    }
+
+    for (int i : arr)
+    {
+        fout << i << " ";
+    }
+
+    fout.close();
+
+    return 0;
 }
