b975b3be71ce41a9d97c61750efa7c32541afc31
Maksim Gorshkov <m.gorshkov96@gmail.com>
2023-05-30 16:09:03 +0300

    Completed lesson06.exercise01

---
+++ ITMO.124-28.Cpp.Lesson06/ITMO.124-28.Cpp.Lesson06.Exercise01/ITMO.124-28.Cpp.Lesson06.Exercise01.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <fstream>
+
+int main()
+{
+    const int size = 150;
+    char buffer[size];
+    
+    std::cout << "Enter poem. Enter '&' to finish." << std::endl;
+    std::cin.getline(buffer, size, '&');
+
+    std::ofstream fout("poem.txt");
+
+    if (!fout)
+    {
+        std::cerr << "Recording failed" << std::endl;
+    }
+
+    fout << buffer;
+    
+    fout.close();
+}
